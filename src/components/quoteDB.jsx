052a8d0af82edac070de8bc79d895ed85b303edd
AliReza <ali.reza.buet@gmail.com>
2023-05-24 08:58:55 +0600

    create quote database component

---
+++ src/components/quoteDB.jsx
@@ -0,0 +1,36 @@
+const quotes = [
+    {
+        "quote": "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        "author": "Nelson Mandela"
+    },
+    {
+        "quote":"Dreams shape the world.",
+        "author":"Neil Gailman"
+    },
+    {
+        "quote":"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        "author": "James Cameron"
+    },
+    {
+        "quote":"The way to get started is to quit talking and begin doing",
+        "author":"Walt Disney"
+    },
+    {
+        "quote":"Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+        "author":"Mother Terresa"
+    },
+    {
+        "quote":"Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+        "author":"Benjamin Franklin"
+    },
+    {
+        "quote":"The future belongs to those who believe in the beauty of their dreams.",
+        "author":"Eleanor Roosevelt"
+    },
+    {
+        "quote":"To create is to live twice.",
+        "author": "Albert Camus"
+    }
+]
+
+export default quotes
